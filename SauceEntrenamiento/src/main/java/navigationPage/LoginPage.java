892a54b744fad65265960a7c58b17aecc52c4a79
jramoncard <li_jramoncr@outlook.com>
2023-11-04 11:46:09 -0600

    Second commit creating TC01

---
+++ SauceEntrenamiento/src/main/java/navigationPage/LoginPage.java
@@ -0,0 +1,30 @@
+package navigationPage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import commonClasses.WrapClass;
+
+public class LoginPage {
+	
+	//Constructor Method to instance the WebDriver, we should create one for each navigation page and ALWAYS should have the name of the MAIN CLASS
+	public LoginPage(WebDriver driver) {
+		PageFactory.initElements(driver,this);   //This line initialized all the web elements of the page even when the page is not loaded 100%
+	}
+	
+	//Find Elements
+	@FindBy(id = "user-name") private WebElement userNameTxt;  //Find an element using id, with the type=private as WebElement called "userNameTxt"
+	@FindBy(id = "password") private WebElement passwordTxt;
+	@FindBy(id = "login-button") private WebElement btnLogin;
+	
+	
+	//Methods to login 
+	public void login(String user, String password) {
+		WrapClass.sendKeys(userNameTxt, user);	
+		WrapClass.sendKeys(passwordTxt, password);
+		WrapClass.click(btnLogin);
+	}
+
+}
---
+++ SauceEntrenamiento/src/test/java/loginTest/TC01__Login.java
@@ -0,0 +1,48 @@
+package loginTest;
+
+import org.testng.annotations.Test;
+
+import commonClasses.DriverSetUp;
+import commonClasses.GlobalVariables;
+import navigationPage.LoginPage;
+
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+
+
+
+public class TC01__Login {
+	
+		//Declare and Initialized an object type WebDriver called "driver" to assign the function inside the method "setDtiver"
+		WebDriver driver = DriverSetUp.setDriver();
+		
+		
+		//Page Objects
+		LoginPage loginPage = new LoginPage(driver);
+		
+		
+	@BeforeTest
+		//Method to start page, calling the browser with the URL saved in GlobalVariables
+		public void startWebPage() {
+			driver.get(GlobalVariables.HOME_PAGE);
+		}
+  
+ 
+	  @Test
+	  public void TC01() {
+		  
+		  loginPage.login(GlobalVariables.STANDARD_USER, GlobalVariables.STANDARD_PASSWORD);
+		  
+	  }
+	  
+	  @AfterTest
+	  public void closeDriver() {
+		  driver.quit();
+	  }
+	  
+	  
+	  
+
+  
+}
